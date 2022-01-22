ec72dc394fff7866f71bc7c0f3035b8784389e82
sromero50 <seba98asd@hotmail.com>
2022-01-22 02:31:57 -0300

    more models

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Movie
+from .models import db, User, Cinema, Schedule, Movie, Ticket, Administrator
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -10,9 +10,13 @@ def setup_admin(app):
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
     
-    # Add your models here, for example this is how we add a the User model to the admin
+    # Add your models here, for example this is how we add a the Usuario model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Administrator, db.session))
     admin.add_view(ModelView(Movie, db.session))
+    admin.add_view(ModelView(Schedule, db.session))
+    admin.add_view(ModelView(Cinema, db.session))
+    admin.add_view(ModelView(Ticket, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -11,12 +11,13 @@ class User(db.Model):
     date_of_birth = db.Column(db.String(80), unique=False, nullable=False)
     phone = db.Column(db.Integer, unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    ticket = db.relationship("Ticket", backref="user")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
-        return {
+       return {
             "id": self.id,
             "email": self.email,
             "name": self.name,
@@ -25,8 +26,24 @@ class User(db.Model):
             "phone": self.phone
         }
 
-class Movie(db.Model):
+class Administrator(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
 
+    def __repr__(self):
+        return '<Administrator %r>' % self.nombre
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "password": self.password
+           }
+
+class Movie(db.Model):
+    schedule = db.relationship("Schedule", backref="movie")
+    cinema = db.relationship("Cinema", backref="movie")
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     synopsis = db.Column(db.String(400), unique=False, nullable=False)
@@ -34,10 +51,10 @@ class Movie(db.Model):
     release_date = db.Column(db.String(120), unique=False, nullable=False)
     duration = db.Column(db.Integer, unique=False, nullable=False)
     director = db.Column(db.String(120), unique=False, nullable=False)
-
+    ticket = db.relationship("Ticket", backref="movie")
 
     def __repr__(self):
-        return '<Linea %r>' % self.name
+        return '<Movie %r>' % self.name
 
     def serialize(self):
         return {
@@ -51,4 +68,75 @@ class Movie(db.Model):
 
                        
         }
-    
+
+class Cinema(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(120), unique=True, nullable=False)
+    latitud = db.Column(db.Float(53), unique=True, nullable=False)
+    longitud = db.Column(db.Float(53), unique=True, nullable=False)
+    schedule = db.relationship("Schedule", backref="cinema")
+    id_movie = db.Column(db.Integer, db.ForeignKey('movie.id'))
+
+    def __repr__(self):
+        return '<Cinema %r>' % self.location
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "location": self.location,
+            "id_movie": self.id_movie,
+            "latitud": self.latitud,
+            "longitud": self.longitud
+           
+        }
+
+class Schedule(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_movie = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    id_cinema = db.Column(db.Integer, db.ForeignKey('cinema.id'))
+    date = db.Column(db.String(80), unique=False, nullable=False)
+    hour = db.Column(db.String(80), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return '<Schedule %r>' % self.hour
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_movie": self.id_movie,
+            "id_cinema": self.id_cinema,
+            "date": self.date,
+            "hour": self.hour
+           
+        }
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_movie = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    hour = db.Column(db.String(80), unique=False, nullable=False)
+    date = db.Column(db.String(80), unique=False, nullable=False)
+    cinema = db.Column(db.String(80), unique=False, nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    code = db.Column(db.String(80), unique=True, nullable=False)
+    seat = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Ticket %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_movie": self.id_movie,
+            "hour": self.hour,
+            "date": self.date,
+            "cinema": self.cinema,
+            "id_user": self.id_user,
+            "code": self.code,
+            "seat": self.seat
+
+
+           
+        }
